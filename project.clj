2bc12c6032ac04bf2fcbdd2e115065919da59e30
Anna Shchiptsova <shchipts@iiasa.ac.at>
2021-05-08 00:33:07 +0200

    Init repo

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.iiasa/dice-simulator "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/shchipts/dice-simulator"
+  :scm {:name "git"
+        :url "https://github.com/shchipts/dice-simulator"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :main nil)
